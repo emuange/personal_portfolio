301b36f4b9d43a30e45a4bc9da3cac68e700deab
emuange <emuange@gmail.com>
2023-07-04 19:13:19 +0300

    Footer and Contact Page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import headerImg from "../assets/img/header-img.svg";
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0); //indicates index of the word currently being displayed on screen
     const [isDeleting, setIsDeleting] = useState(false); //is the word being typed or deleted?
-    const toRotate = [ "Web Developer", "Web Designer", "Photographer"]; //Words in rotation to be displayed and deleted
+    const toRotate = [ "Web Developer", "Game Developer", "ML & Data Enthusiast"]; //Words in rotation to be displayed and deleted
     const [text, setText] = useState('');  //indicates the portion of the word being displayed eg W, We, Web
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -47,7 +47,7 @@ export const Banner = () => {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'Hello My name is Emmanuel. '}<span className="wrap">{text}</span></h1>
+                        <h1>{"Hi, I'm Emmanuel. "}<br></br><span className="wrap">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
                     </Col>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import contactImg from "../assets/img/contact-img.svg"
 
 export const Contact = () => 
 {
-    const forInitialDetails = 
+    const formInitialDetails = 
     {
         firstName: '',
         lastName: '',
@@ -25,10 +25,26 @@ export const Contact = () =>
         })
     }
 
-    const handleSubmit = () =>
+    const handleSubmit = async(e) =>
     {
-        
-    }
+        e.preventDefault();
+        setButtonText('Sending...');
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-type": "Application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formDetails),
+        })
+        setButtonText("Send");
+        let result = response.json();
+        setFormDetails(formInitialDetails);
+        if (result.code === 200){
+            setStatus({ success: true, message: 'Message sent successfully'});
+        } else {
+            setStatus({ success: false, message: 'Something went wrong, please try again later'})
+        }
+    };
 
     return (
         <section className="contact" id="connect">
