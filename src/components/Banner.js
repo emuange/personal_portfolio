564aafb9f17f474d73cbfc9d4520d685b045ef6b
emuange <emuange@gmail.com>
2023-07-04 21:40:21 +0300

    finishing touches. Changed images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/Spaceman.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
 
 /* Banner animation Code*/
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0); //indicates index of the word currently being displayed on screen
     const [isDeleting, setIsDeleting] = useState(false); //is the word being typed or deleted?
-    const toRotate = [ "Web Developer", "Game Developer", "ML & Data Enthusiast"]; //Words in rotation to be displayed and deleted
+    const toRotate = [ "Web Developer", "Graphic Designer", "ML & Data Enthusiast"]; //Words in rotation to be displayed and deleted
     const [text, setText] = useState('');  //indicates the portion of the word being displayed eg W, We, Web
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const period = 2000;
+    const [delta, setDelta] = useState(50);
+    const period = 1000;
 
     useEffect(() => {
         let ticker = setInterval(() => {
@@ -46,10 +49,15 @@ export const Banner = () => {
             <Container>
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility>
+                        {({ isVisible }) =>
+                        <div className={isVisible ? "animate__animated animate__fadeIn" : ""}> 
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{"Hi, I'm Emmanuel. "}<br></br><span className="wrap">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
+                        </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img"/>
